83814635d3e58441ca7964e35e875a0268bc02e1
Clay Stewart <claytonmstewart@gmail.com>
2013-10-24 11:21:45 -0500

    add tests for api calls in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,9 @@ class HomeController < ApplicationController
   def fetch_event_api_data(artist)
     response = Net::HTTP.get_response(URI('http://api.seatgeek.com/2/events?performers.slug=' + artist))
     api_data = JSON.parse response.body
+  rescue Timeout::Error => e
+    puts "SeatGeek api call error: #{e}"
+    { :events => [] }
   end
 
   def process_api_data(data)
@@ -16,8 +19,20 @@ class HomeController < ApplicationController
     # pp data
 
     # TODO:
-    lowest_price = -1
-    highest_price = -1
+    lowest_price_array = []
+    highest_price_array = []
+    data["events"].each do |a| 
+      lowest_price_array << a["stats"]["lowest_price"] if a["stats"]["lowest_price"] != nil
+    end
+    data["events"].each do |a| 
+      highest_price_array << a["stats"]["highest_price"] if a["stats"]["lowest_price"] != nil
+    end
+
+    lowest_price_array.sort! {|a,b| a <=> b}
+    highest_price_array.sort! {|a,b| b <=> a}
+
+    lowest_price = lowest_price_array[0]
+    highest_price = highest_price_array[0]
     {
       :lowest_price => lowest_price,
       :highest_price => highest_price
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,10 +3,36 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    
+    it "fetches api data based on an artist parameter" do
+      expect(@controller).to receive(:fetch_event_api_data).with('the-bad-plus')
+      expect(@controller).to receive(:process_api_data)
+
+      get 'index', { :artist => 'the-bad-plus' }
+      expect(response).to be_success
     end
+
+    it "calculates event data based on seatgeek's api response" do
+      VCR.use_cassette('bieber_events') do
+        api_data = @controller.fetch_event_api_data('justin-bieber')
+
+        data = @controller.process_api_data(api_data)
+        #pending "Need to run test once with VCR first"
+        expect(data).to be_a(Hash)
+        expect(data[:lowest_price]).to eq(20)
+        expect(data[:highest_price]).to eq(7118)
+      end
+    end
+
+    it "doesn't die when the api service goes down" do
+      exception = Timeout::Error.new 'Service timed out'
+      expect(Net::HTTP).to receive(:get_response).and_raise(exception)
+
+      # Makes sure the fetch method handles an exception and returns useable data anyway
+      api_data = @controller.fetch_event_api_data('justin-bieber')
+      expect(api_data[:events]).to eq([])
+    end
+
   end
 
 end
